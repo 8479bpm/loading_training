943301b46fab5242844569d308f6ddd0a13aec25
8479Bpm <pauline.froidure@gmail.com>
2025-01-02 21:17:17 +0100

    WIP

---
+++ learn_how_to_swim/C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: 8479bpm <8479bpm@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/02 17:53:53 by 8479bpm           #+#    #+#             */
+/*   Updated: 2025/01/02 17:53:55 by 8479bpm          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	index;
+
+	index = 0;
+	if (str[0] == '\0')
+	{
+		return (1);
+	}
+	while (str[index] != '\0')
+	{
+		if (!(str[index] >= 'a' && str[index] <= 'z')
+		|| !(str[index] >= 'A' && str[index] <= 'Z'))
+		{
+			return (0);
+		}
+		index++;
+	}
+	return (1);
+}
+/*
+int	main(void)
+{
+	char	*string1;
+	int		is_alpha;
+
+	string1 = "ZERfbfg@!:\ndthh";
+	is_alpha = ft_str_is_alpha(string1);
+	printf("is alpha ? Yes = 0, No = 1, this string is : %d \n", is_alpha);
+	return (0);
+}
+*/
