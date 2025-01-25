c817c202f0c09284e8f4e610364d6f8c914c9759
8479Bpm <pauline.froidure@gmail.com>
2025-01-25 23:36:41 +0100

    WIP

---
+++ learn_how_to_swim/C09/ex01/srcs/ft_strlen.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: 8479bpm <8479bpm@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/05 14:53:40 by 8479bpm           #+#    #+#             */
+/*   Updated: 2025/01/05 14:53:42 by 8479bpm          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "../includes/ft.h"
+
+int	ft_strlen(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+	{
+		index++;
+	}
+	return (index);
+}
+/*
+int	main(void)
+{
+	char str1[] = "dis moi combien je mesure";
+	int result;
+
+	result = ft_strlen(str1);
+	printf("%d", result);
+	return (0);
+}
+*/
